ae6b79b34cedffb2c339bc71dc446a36733c3a97
Rachel Xu <rachel960122@hotmail.com>
2016-03-18 00:32:58 -0400

    Done.

--- javacs-lab00/src/com/flatironschool/javacs/HelloWorld.java
+++ javacs-lab00/src/com/flatironschool/javacs/HelloWorld.java
@@ -2,10 +2,14 @@ package com.flatironschool.javacs;
 
 public class HelloWorld {
     public static Double getVersion() {
-        // I won't work!
+    	String javav = System.getProperty("java.version");
+    	int deciPos1 = javav.indexOf('.');
+    	int deciPos2 = javav.indexOf('.', deciPos1 + 1);
+    	String v = javav.substring(0, deciPos2);
+        return Double.parseDouble(v);
     }
 
     public static void main(String[] args) {
-	    // you can test the output of getVersion() here
+	    System.out.println(getVersion());
     }
 }
